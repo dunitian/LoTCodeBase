c5445eb74c24559c2792c42b580586a5be9bf7aa
dunitian <1054186320@qq.com>
2016-04-20 12:39:10 +0800

    1.枚举默认之为0

---
+++ NetCode/4.原理系列/0.基础/1.枚举默认之为0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EnumStart
+{
+    enum Test
+    {
+        mmd,
+        dzy,
+        atm,
+        ymt
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp = Convert.ToInt32(Test.mmd);
+            Console.WriteLine(temp);
+            Console.ReadKey();
+        }
+    }
+}
