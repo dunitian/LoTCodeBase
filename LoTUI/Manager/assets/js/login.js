39eb128f064ef1a722a9dd452349df7c09bc6f6f
dunitian <1054186320@qq.com>
2016-05-19 15:03:45 +0800

    登录修改为通用版本，Menu demo添加头像

--- LoTUI/Manager/assets/js/login.js
+++ LoTUI/Manager/assets/js/login.js
@@ -1,27 +1,34 @@
-jQuery(document).ready(function() {
-    $('.page-container form').submit(function(){
-        var username = $(this).find('.username').val();
-        var password = $(this).find('.password').val();
-        if(username == '') {
-            $(this).find('.error').fadeOut('fast', function(){
+jQuery(document).ready(function () {
+    $('#btn').click(function () {
+        var username = $('input[name=userName]').val();
+        var password = $('input[name=passWord]').val();
+        if (username == '') {
+            $(this).parent().find('.error').fadeOut('fast', function () {
                 $(this).css('top', '27px');
             });
-            $(this).find('.error').fadeIn('fast', function(){
+            $(this).parent().find('.error').fadeIn('fast', function () {
                 $(this).parent().find('.username').focus();
             });
             return false;
         }
-        if(password == '') {
-            $(this).find('.error').fadeOut('fast', function(){
+        if (password == '') {
+            $(this).parent().find('.error').fadeOut('fast', function () {
                 $(this).css('top', '96px');
             });
-            $(this).find('.error').fadeIn('fast', function(){
+            $(this).parent().find('.error').fadeIn('fast', function () {
                 $(this).parent().find('.password').focus();
             });
             return false;
         }
+        //$.post('/Manager/Index.html', { username: userName, password: passWord }, function (data) {
+        //    if (data != 'false') {
+        //        location.href = '/Manager/Index.html'
+        //    }
+        //});
+        location.href = '/Manager/Index.html'
     });
-    $('.page-container form .username, .page-container form .password').keyup(function(){
+    
+    $('input').keyup(function () {
         $(this).parent().find('.error').fadeOut('fast');
     });
 });
