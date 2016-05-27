cfe37c2ac9fb398d9c325c4b2d489b2c4730b935
dunitian <1054186320@qq.com>
2016-05-27 17:10:04 +0800

    LoTUploader封装

---
+++ NetCode/3.常用技能/02.uploader系列/02.LoTUploader/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _02.LoTUploader.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 图片上传
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult Upload(HttpPostedFileBase file)
+        {
+            if (file == null) { return Json(new { status = false, msg = "图片提交失败" }); }
+            string filterStr = ".gif,.jpg,.jpeg,.bmp,.png";
+            string fileExt = Path.GetExtension(file.FileName).ToLower();
+            if (!filterStr.Contains(fileExt)) { return Json(new { status = false, msg = "图片格式不对" }); }
+            if (file.ContentLength > 10485760) { return Json(new { status = false, msg = "文件10M以内" }); }
+
+            //todo: md5判断一下文件是否已经上传过,如果已经上传直接返回 return Json(new { status = true, msg = sqlPath });
+
+            string path = string.Format("{0}/{1}", "/lotFiles", DateTime.Now.ToString("yyyy-MM-dd"));
+            string fileName = string.Format("{0}{1}", Guid.NewGuid().ToString("N"), fileExt);
+            string sqlPath = string.Format("{0}{1}", path, fileName);
+            string dirPath = Server.MapPath(path);
+
+            if (!Directory.Exists(dirPath)) { Directory.CreateDirectory(dirPath); }
+            try
+            {
+                file.SaveAs(Path.Combine(dirPath, fileName));
+                //todo: 未来写存数据库的Code
+            }
+            catch { return Json(new { status = false, msg = "图片保存失败" }); }
+            return Json(new { status = true, msg = sqlPath });
+        }
+    }
+}
---
+++ NetCode/3.常用技能/02.uploader系列/02.LoTUploader/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_02.LoTUploader.MvcApplication" Language="C#" %>
