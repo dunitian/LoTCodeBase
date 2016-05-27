95209cb7351756a01baa24fae0d7ba040ff17e4f
dunitian <1054186320@qq.com>
2016-05-27 13:50:45 +0800

    空文件处理

--- NetCode/3.常用技能/02.uploader系列/01.Webuploader/Controllers/HomeController.cs
+++ NetCode/3.常用技能/02.uploader系列/01.Webuploader/Controllers/HomeController.cs
@@ -13,10 +13,13 @@ namespace _01.Webuploader.Controllers
         /// <returns></returns>
         public JsonResult Upload(HttpPostedFileBase file)
         {
+            if (file == null) { return Json(new { status = false, msg = "图片提交失败" }); }
             string filterStr = ".gif,.jpg,.jpeg,.bmp,.png";
             string fileExt = Path.GetExtension(file.FileName).ToLower();
             if (!filterStr.Contains(fileExt)) { return Json(new { status = false, msg = "图片格式不对" }); }
             if (file.ContentLength > 10485760) { return Json(new { status = false, msg = "文件10M以内" }); }
+
+            //todo: md5判断一下文件是否已经上传过,如果已经上传直接返回 return Json(new { status = true, msg = sqlPath });
 
             string path = string.Format("{0}/{1}", "/lotFiles", DateTime.Now.ToString("yyyy-MM-dd"));
             string fileName = string.Format("{0}{1}", Guid.NewGuid().ToString("N"), fileExt);
@@ -29,8 +32,8 @@ namespace _01.Webuploader.Controllers
                 file.SaveAs(Path.Combine(dirPath, fileName));
                 //todo: 未来写存数据库的Code
             }
-            catch { return Json(new { status = false, msg = "文件保存失败" }); }
-            return Json(new { status = true });
+            catch { return Json(new { status = false, msg = "图片保存失败" }); }
+            return Json(new { status = true, msg = sqlPath });
         }
     }
 }
