7fe246fee9a520d0cf1e7ce6275ae94bd415f921
dunitian <1054186320@qq.com>
2016-06-22 10:40:45 +0800

    整理归纳

--- NetCode/3.常用技能/05.AutoMapper/_AutoMapper/AutoMapper/AutoMapperExt.cs
+++ NetCode/3.常用技能/05.AutoMapper/_AutoMapper/AutoMapper/AutoMapperExt.cs
@@ -9,8 +9,9 @@ namespace _AutoMapper
     /// </summary>
     public static class AutoMapperExt
     {
+        #region 由AutoMapper创建目标对象
         /// <summary>
-        ///  类型映射
+        ///  类型映射-由AutoMapper创建目标对象
         /// </summary>
         public static T MapTo<T>(this object obj)
         {
@@ -19,16 +20,18 @@ namespace _AutoMapper
         }
 
         /// <summary>
-        /// 集合列表类型映射
+        /// 集合列表类型映射-由AutoMapper创建目标对象
         /// </summary>
         public static IEnumerable<TDestination> MapToList<TDestination>(this IEnumerable source)
         {
             if (source == null) return new List<TDestination>();
             return Mapper.Map<IEnumerable<TDestination>>(source);
-        }
+        } 
+        #endregion
 
+        #region 把源对象中的属性值合并/覆盖到目标对象
         /// <summary>
-        /// 类型映射
+        /// 类型映射-把源对象中的属性值合并/覆盖到目标对象
         /// </summary>
         public static TDestination MapTo<TSource, TDestination>(this TSource source, TDestination destination) where TSource : class where TDestination : class
         {
@@ -37,12 +40,23 @@ namespace _AutoMapper
         }
 
         /// <summary>
+        /// 集合列表类型映射-把源对象中的属性值合并/覆盖到目标对象
+        /// </summary>
+        public static IEnumerable<TDestination> MapToList<TSource, TDestination>(this IEnumerable<TSource> source, IEnumerable<TDestination> destination)
+        {
+            return Mapper.Map(source, destination);
+        }
+        #endregion
+
+        #region DataReader映射
+        /// <summary>
         /// DataReader映射
         /// </summary>
         public static IEnumerable<T> DataReaderMapTo<T>(this System.Data.IDataReader reader)
         {
             Mapper.Initialize(cfg => cfg.CreateMap<System.Data.IDataReader, IEnumerable<T>>());
             return Mapper.Map<System.Data.IDataReader, IEnumerable<T>>(reader);
-        }
+        } 
+        #endregion
     }
 }
