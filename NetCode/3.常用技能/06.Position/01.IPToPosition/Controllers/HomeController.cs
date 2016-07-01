922c88dc23a83424e905c91596bf87ed9102d82e
dunitian <1054186320@qq.com>
2016-07-01 20:22:49 +0800

    add 01.IPToPosition

---
+++ NetCode/3.常用技能/06.Position/01.IPToPosition/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace IPToPosition.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// IP归属地查询
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            IPAndPositionHelper.EnableFileWatch = true; // 默认值为：false，如果为true将会检查ip库文件的变化自动reload数据
+
+            string path = Server.MapPath("/APP_Data/IPDB20151001.dat");
+            if (string.IsNullOrEmpty(path) || System.IO.Path.GetExtension(path) != ".dat")
+            {
+                return View();
+            }
+
+            IPAndPositionHelper.Load(path);//加载
+
+            //JsonHelper.ToJson(IPAndPositionHelper.Find("210.21.113.236"));
+            //ViewBag.infos = IPAndPositionHelper.Find("183.209.0.207");//查询
+            var ipStr = Request.UserHostAddress;
+            if (ipStr == "::1") ipStr = "127.0.0.1";
+            ViewBag.infos = IPAndPositionHelper.Find(ipStr);
+
+            return View();
+        }
+    }
+}
