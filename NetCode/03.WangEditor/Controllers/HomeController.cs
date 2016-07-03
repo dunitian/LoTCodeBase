7058dae949b9bb65b24e80343897017eec4e4bbd
dunitian <1054186320@qq.com>
2016-07-03 22:06:33 +0800

    路径补全（少了个 /），WangEditor 完整Demo

--- NetCode/03.WangEditor/Controllers/HomeController.cs
+++ NetCode/03.WangEditor/Controllers/HomeController.cs
@@ -3,23 +3,30 @@ using System.IO;
 using System.Web;
 using System.Web.Mvc;
 
-namespace _01.Webuploader.Controllers
+namespace _03.WangEditor.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
         /// <summary>
         /// 图片上传
         /// </summary>
         /// <returns></returns>
-        public JsonResult Upload(HttpPostedFileBase file)
+        public ContentResult Upload(HttpPostedFileBase file)
         {
-            if (file == null) { return Json(new { status = false, msg = "图片提交失败" }); }
+            if (file == null) { return Content("error|文件不能为空"); }
             string filterStr = ".gif,.jpg,.jpeg,.bmp,.png";
+            //如果是上传文件，再添加其他格式即可
             string fileExt = Path.GetExtension(file.FileName).ToLower();
-            if (!filterStr.Contains(fileExt)) { return Json(new { status = false, msg = "图片格式不对" }); }
-            if (file.ContentLength > 10485760) { return Json(new { status = false, msg = "文件10M以内" }); }
+            if (!filterStr.Contains(fileExt)) { return Content("error|文件后缀不对"); }
+            if (file.ContentLength > 10485760) { return Content("error|文件10M以内"); }
 
-            //todo: md5判断一下文件是否已经上传过,如果已经上传直接返回 return Json(new { status = true, msg = sqlPath });
+            //todo: md5判断一下文件是否已经上传过,如果已经上传直接返回 return Content(sqlPath#缩略图地址);
 
             string path = string.Format("{0}/{1}", "/lotFiles", DateTime.Now.ToString("yyyy-MM-dd"));
             string fileName = string.Format("{0}{1}", Guid.NewGuid().ToString("N"), fileExt);
@@ -29,12 +36,13 @@ namespace _01.Webuploader.Controllers
             if (!Directory.Exists(dirPath)) { Directory.CreateDirectory(dirPath); }
             try
             {
-                //todo：缩略图
+                //todo：未来写缩略图的代码
+
                 file.SaveAs(Path.Combine(dirPath, fileName));
                 //todo: 未来写存数据库的Code
             }
-            catch { return Json(new { status = false, msg = "图片保存失败" }); }
-            return Json(new { status = true, msg = sqlPath });
+            catch { return Content("error|文件保存失败"); }
+            return Content(string.Format("{0}#{1}", sqlPath, "缩略图地址"));
         }
     }
 }
---
+++ NetCode/03.WangEditor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_03.WangEditor.MvcApplication" Language="C#" %>
