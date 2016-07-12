a11eacfe8db017ff71b6acb4a0f075e06cc96059
dunitian <1054186320@qq.com>
2016-07-12 21:24:30 +0800

    Email Base Skill

--- NetCode/0.知识拓展/01.网络编程/3.System.Net.Mail/3.3.SmtpClient/Form1.cs
+++ NetCode/0.知识拓展/01.网络编程/3.System.Net.Mail/3.3.SmtpClient/Form1.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +9,16 @@ namespace _3._3.SmtpClient
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task.Run(SendMsg);
+        }
+        public async Task SendMsg()
+        {
+            bool b = await EmailHelper.SendAsync("1054186320@qq.com", "test标题", "test内容");
+            MessageBox.Show(b.ToString());
         }
     }
 }
