416e219ec68235b57e3e2f357573ae8add0e4256
dunitian <1054186320@qq.com>
2016-07-18 21:58:13 +0800

    AutoFac

---
+++ NetCode/3.常用技能/8.AutoFac/1.AutoFac/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1.AutoFac.MvcApplication" Language="C#" %>
--- NetCode/3.常用技能/8.AutoFac/1.AutoFac/Properties/AssemblyInfo.cs
+++ NetCode/3.常用技能/8.AutoFac/1.AutoFac/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过下列特性集
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("WebEmail")]
+[assembly: AssemblyTitle("_1.AutoFac")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebEmail")]
+[assembly: AssemblyProduct("_1.AutoFac")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
-[assembly: Guid("d8ba09eb-79e5-4907-b717-a9bd1f8e0f03")]
+[assembly: Guid("01ea2ae5-7b14-4665-a658-68ae12b83804")]
 
 // 程序集的版本信息由下列四个值组成:
 //
