b93a5130c87a2b714f277351843f5deb52a83a93
dunitian <1054186320@qq.com>
2016-08-01 10:32:08 +0800

    add FormController

---
+++ NetCode/6.网页基础/BMVC5/MVC5Base/Controllers/FormController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MVC5Base.Controllers
+{
+    public class FormController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
