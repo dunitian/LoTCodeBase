7c2f6faa0aafcac0d3de29b889d0d21fe16651dc
dunitian <1054186320@qq.com>
2016-08-19 22:45:04 +0800

    过度提交

---
+++ NetCode/6.网页基础/BMVC5/MVC5Base/Controllers/ModelController.cs
@@ -0,0 +1,28 @@
+﻿using MVC5Base.Models;
+using System.Web.Mvc;
+
+namespace MVC5Base.Controllers
+{
+    public class ModelController : Controller
+    {
+        /// <summary>
+        /// 模型常用特性和过度提交防御
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            ViewBag.Product = new Product {ID="SJ008-1012", Name = "小馒头", Count = 23, Price = 1.25 };
+            return View();
+        }
+        /// <summary>
+        /// 偷偷的把价格改了
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public ActionResult Put(Product model)
+        {
+            ViewBag.Product = model;
+            return View("~/Views/Model/Index.cshtml");
+        }
+    }
+}
---
+++ NetCode/6.网页基础/BMVC5/MVC5Base/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MVC5Base.Models
+{
+    public partial class Product
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public double Price { get; set; }
+    }
+}
