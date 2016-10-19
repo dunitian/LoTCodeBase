05228684bd662b218e23e76079f018f2164faea4
dunitian <1054186320@qq.com>
2016-10-19 15:29:41 +0800

    质数

--- NetCode/1.面向过程/01.基础/09.质数练习/Program.cs
+++ NetCode/1.面向过程/01.基础/09.质数练习/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _09.质数练习
 {
     class Program
     {
+        /// <summary>
+        /// 输出0~100中所有的质数
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (IsMassNum(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// 是否是质数
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        private static bool IsMassNum(int i)
+        {
+            for (int j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
