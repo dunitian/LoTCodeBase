9853599d4b4c9634ae3309761e9b5dee21cd043b
dunitian <1054186320@qq.com>
2016-12-04 15:30:41 +0800

    全局ID，demo演示

---
+++ NetCode/3.常用技能/01.Snowflake/Program.cs
@@ -0,0 +1,64 @@
+﻿using Snowflake.Net;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snowflake.ZConsole
+{
+    /// <summary>
+    /// 详细说明：http://www.cnblogs.com/dunitian/p/6130543.html
+    /// </summary>
+    class Program
+    {
+        private static int N = 1000000;
+        private static HashSet<long> set = new HashSet<long>();
+        //private static IdWorker worker = new IdWorker(1, 1);
+        private static int taskCount = 0;
+
+        static void Main(string[] args)
+        {
+            Task.Run(() => GetID());
+            Task.Run(() => GetID());
+            Task.Run(() => GetID());
+            Task.Run(() => GetID());
+            Task.Run(() => GetID());
+
+            Task.Run(() => Printf());
+            Console.ReadKey();
+        }
+
+        private static void Printf()
+        {
+            while (taskCount != 5)
+            {
+                Console.WriteLine("...");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine(set.Count == N * taskCount);
+        }
+
+        private static object o = new object();
+        private static void GetID()
+        {
+            for (var i = 0; i < N; i++)
+            {
+                var id = IdWorker.Init().NextId();//worker.NextId();
+
+                lock (o)
+                {
+                    if (set.Contains(id))
+                    {
+                        Console.WriteLine("发现重复项 : {0}", id);
+                    }
+                    else
+                    {
+                        set.Add(id);
+                    }
+                }
+
+            }
+            Console.WriteLine($"任务{++taskCount}完成");
+        }
+    }
+}
---
+++ NetCode/6.网页基础/BMVC5/MVC5Base/Controllers/SessionController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace MVC5Base.Controllers
+{
+    public class SessionController : Controller
+    {
+        /// <summary>
+        /// Session状态服务器
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
