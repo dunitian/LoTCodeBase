97cfd01a259af8d629efdf300a49d44445cc417f
dunitian <1054186320@qq.com>
2016-12-04 16:29:38 +0800

    进程外Session，登录页面结束，下面步入正轨

--- NetCode/6.网页基础/BMVC5/MVC5Base/Controllers/SessionController.cs
+++ NetCode/6.网页基础/BMVC5/MVC5Base/Controllers/SessionController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using MVC5Base.Models;
+using System.Web.Mvc;
 
 namespace MVC5Base.Controllers
 {
@@ -10,7 +11,35 @@ namespace MVC5Base.Controllers
         /// <returns></returns>
         public ActionResult Index()
         {
+            if (Session["user"] == null)
+            {
+                return RedirectToAction("Login", "Session");
+            }
             return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserInfo model)
+        {
+            if (ModelState.IsValid)
+            {
+                //登录成功跳转到Index
+                if (model.Name == "dnt" && model.Pass == "dnt")
+                {
+                    Session["user"] = "dnt";
+                    return RedirectToAction("Index", "Session");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "用户名或密码错误（用户名和密码都是dnt）");
+                }
+            }
+
+            return View(model);//如果出错，则重新显示表单
         }
     }
 }
